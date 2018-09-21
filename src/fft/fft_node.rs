33144208cd04668be44cc2be84fb9e132a7f2761
ostrosco <ostrosco@fastmail.fm>
2018-09-20 23:52:23 -0600

    Initial experimentation on creating init function for new nodes

---
+++ src/fft/fft_node.rs
@@ -0,0 +1,22 @@
+#[macro_use]
+use node::Node;
+use crossbeam::{Receiver, Sender};
+
+create_node!(FFTNode, Fn(Vec<i16>) -> Vec<i16>, recv);
+
+pub fn init() -> impl Node
+where
+{
+    FFTNode::new(|x| x)
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_fft_node() {
+        use node::Node;
+        use crossbeam::{Receiver, Sender};
+        use fft::fft_node;
+        let fft_node = fft_node::init();
+    }
+}
