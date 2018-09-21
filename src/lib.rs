214741e51014f603252ea08cbc2a2cfa24a365e2
ostrosco <ostrosco@fastmail.fm>
2018-09-20 21:19:09 -0600

    Creating initial skeleton for the project
    - Created a few folders that will encapsulate the functionality that
      we're hoping to deliver.

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,9 @@ extern crate rand;
 #[macro_use]
 pub mod node;
 pub mod modulation;
+pub mod filter;
+pub mod fft;
+pub mod hardware;
+pub mod util;
+
 pub use crossbeam::{channel, Receiver, Sender};
