7f799d328a267218c00d2f94a616a48d5fda6f14
ostrosco <ostrosco@fastmail.fm>
2018-09-22 20:33:36 -0600

    Updating create_aggregate_node! to use the same format as create_node!
    - create_aggregate_node! now takes the same inputs as create_node! does.
      This provides a uniform interface between the two nodes.

--- src/node/mod.rs
+++ src/node/mod.rs
@@ -91,7 +91,8 @@ pub trait Node {
 /// ```
 #[macro_export]
 macro_rules! create_node {
-    ($name:ident: $out:ty, [$($state:ident: $type:ty),*], [$($recv:ident: $in:ty),*], $func:expr) => {
+    ($name:ident: $out:ty, [$($state:ident: $type:ty),*],
+     [$($recv:ident: $in:ty),*], $func:expr) => {
         pub struct $name {
             $(
                 pub $recv: Option<Receiver<$in>>,
@@ -140,71 +141,33 @@ macro_rules! create_node {
 /// the output of the function is not None.
 #[macro_export]
 macro_rules! create_aggregate_node {
-    ($name:ident, FnMut() -> Option<$out:ty>) => {
-        pub struct $name<F>
-        where
-            F: FnMut() -> Option<$out>,
-        {
-            pub sender: Vec<Sender<$out>>,
-            func: F,
-        }
-
-        impl<F> $name<F>
-        where
-            F: FnMut() -> Option<$out>,
-        {
-            pub fn new(func: F) -> $name<F> {
-                $name {
-                    sender: vec![],
-                    func
-                }
-            }
-        }
-
-        impl<F> Node for $name<F>
-        where
-            F: FnMut() -> Option<$out>,
-        {
-            fn run_node(&mut self) {
-                if let Some(res) = (self.func)() {
-                    for send in &self.sender {
-                        send.send(res.clone());
-                    }
-                }
-            }
-        }
-    };
-    ($name:ident, FnMut($($in:ty),+) -> Option<$out:ty>, $($recv:ident),+) => {
-        pub struct $name<F>
-        where
-            F: FnMut($($in),+) -> Option<$out>,
-        {
+    ($name:ident: Option<$out:ty>, [$($state:ident: $type:ty),*],
+     [$($recv:ident: $in:ty),*], $func:expr) => {
+        pub struct $name {
             $(
                 pub $recv: Option<Receiver<$in>>,
             )*
             pub sender: Vec<Sender<$out>>,
-            func: F,
+            $(
+                pub $state: $type,
+            )*
         }
 
-        impl<F> $name<F>
-        where
-            F: FnMut($($in),+) -> Option<$out>,
-        {
-            pub fn new(func: F) -> $name<F> {
+        impl $name {
+            pub fn new($($state: $type,)*) -> $name {
                 $name {
                     $(
                         $recv: None,
                     )*
                     sender: vec![],
-                    func
+                    $(
+                        $state,
+                    )*
                 }
             }
         }
 
-        impl<F> Node for $name<F>
-        where
-            F: FnMut($($in),+) -> Option<$out>,
-        {
+        impl Node for $name {
             fn run_node(&mut self) {
                 $(
                     let $recv = match self.$recv {
@@ -212,7 +175,7 @@ macro_rules! create_aggregate_node {
                         None => return,
                     };
                 )*
-                if let Some(res) = (self.func)($($recv,)+) {
+                if let Some(res) = ($func)(&mut *self, $($recv,)*) {
                     for send in &self.sender {
                         send.send(res.clone());
                     }
@@ -325,41 +288,42 @@ mod test {
         use std::thread;
         use std::time::Duration;
 
-        create_aggregate_node!(Node1, FnMut() -> Option<Arc<Vec<u32>>>);
         create_aggregate_node!(
-            Node2,
-            FnMut(Arc<Vec<u32>>) -> Option<Arc<Vec<u32>>>,
-            recv1
+            Node1: Option<Arc<Vec<u32>>>,
+            [agg: Vec<u32>],
+            [],
+            |node: &mut Node1| if node.agg.len() < 2 {
+                node.agg.push(1);
+                None
+            } else {
+                let val = node.agg.clone();
+                node.agg = vec![];
+                Some(Arc::new(val))
+            }
+        );
+        create_aggregate_node!(
+            Node2: Option<Arc<Vec<u32>>>,
+            [],
+            [recv1: Arc<Vec<u32>>],
+            |_, x: Arc<Vec<u32>>| {
+                let mut y = Arc::clone(&x);
+                for z in Arc::make_mut(&mut y).iter_mut() {
+                    *z = *z + 1;
+                }
+                Some(y)
+            }
         );
         create_node!(
             Node3: (),
             [],
             [recv2: Arc<Vec<u32>>],
-            {
-                |_, x: Arc<Vec<u32>>| {
-                    assert_eq!(*x, vec![2, 2]);
-                }
+            |_, x: Arc<Vec<u32>>| {
+                assert_eq!(*x, vec![2, 2]);
             }
         );
 
-        let mut agg = Vec::new();
-        let mut node1 = Node1::new(move || {
-            if agg.len() < 2 {
-                agg.push(1);
-                None
-            } else {
-                let val = agg.clone();
-                agg = vec![];
-                Some(Arc::new(val))
-            }
-        });
-        let mut node2 = Node2::new(|x| {
-            let mut y = Arc::clone(&x);
-            for z in Arc::make_mut(&mut y).iter_mut() {
-                *z = *z + 1;
-            }
-            Some(y)
-        });
+        let mut node1 = Node1::new(Vec::new());
+        let mut node2 = Node2::new();
         let mut node3 = Node3::new();
 
         connect_nodes!(node1, node2, recv1);
@@ -386,11 +350,22 @@ mod test {
         use std::thread;
         use std::time::Duration;
 
-        create_aggregate_node!(Node1, FnMut() -> Option<Arc<Vec<i16>>>);
-        create_aggregate_node!(
-            Node2,
-            FnMut(Arc<Vec<i16>>) -> Option<Arc<Vec<i16>>>,
-            recv1
+        create_node!(Node1: Arc<Vec<i16>>, [], [], |_| {
+            let mut random = vec![0i16; 10000];
+            thread_rng().fill(random.as_mut_slice());
+            Arc::new(random)
+        });
+        create_node!(
+            Node2: Arc<Vec<i16>>,
+            [],
+            [recv1: Arc<Vec<i16>>],
+            |_, x: Arc<Vec<i16>>| {
+                let mut y = Arc::clone(&x);
+                for z in Arc::make_mut(&mut y).iter_mut() {
+                    *z = z.saturating_add(1);
+                }
+                y
+            }
         );
         create_node!(
             Node3: (),
@@ -404,18 +379,8 @@ mod test {
             }
         );
 
-        let mut node1 = Node1::new(move || {
-            let mut random = vec![0i16; 10000];
-            thread_rng().fill(random.as_mut_slice());
-            Some(Arc::new(random))
-        });
-        let mut node2 = Node2::new(|x| {
-            let mut y = Arc::clone(&x);
-            for z in Arc::make_mut(&mut y).iter_mut() {
-                *z = z.saturating_add(1);
-            }
-            Some(y)
-        });
+        let mut node1 = Node1::new();
+        let mut node2 = Node2::new();
         let mut node3 = Node3::new(0);
 
         connect_nodes!(node1, node2, recv1);
@@ -445,20 +410,13 @@ mod test {
             DoubleInputNode: f32,
             [],
             [recv1: u32, recv2: f64],
-            |_, x: u32, y: f64| {
-                (x as f64 + y) as f32
-            }
+            |_, x: u32, y: f64| (x as f64 + y) as f32
         );
 
         // Create a node to check the value.
-        create_node!(
-            CheckNode: (),
-            [],
-            [recv: f32],
-            |_, x: f32| {
-                assert_eq!(x, 3.0, "Node didn't work!");
-            }
-        );
+        create_node!(CheckNode: (), [], [recv: f32], |_, x: f32| {
+            assert_eq!(x, 3.0, "Node didn't work!");
+        });
 
         // Now, you can instantiate your nodes as usual.
         let mut node1 = NoInputNode::new();
