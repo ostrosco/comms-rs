8306c279a73996f99ab42eefab9e767e5ae78525
Kyle Harris <kaharrs2@gmail.com>
2018-10-03 07:53:46 -0600

    FIR Batch Filtering
    This commit is in a non-working state, I'm getting a weird macro
    error I don't understand.  I think once that is sorted it should
    work though.  Looking for assistance on that error.

--- src/fir/fir_node.rs
+++ src/fir/fir_node.rs
@@ -13,6 +13,15 @@ create_node!(
     |node: &mut FirNode, input: Complex<i16>| node.run(input)
 );
 
+/// A node that implements a generic FIR filter.  Operates on a batch of
+/// samples at a time.
+create_node!(
+    BatchFirNode: Vec<Complex<i16>>,
+    [taps: Vec<Complex<i16>>, state: Vec<Complex<i16>>],
+    [input: Vec<Complex<i16>>],
+    |node: &mut BatchFirNode, input: Vec<Complex<i16>>| node.run(input)
+);
+
 /// Implementation of run for the FirNode.
 impl FirNode {
     fn run(&mut self, input: Complex<i16>) -> Complex<i16> {
@@ -24,13 +33,37 @@ impl FirNode {
     }
 }
 
+/// Implementation of run for the BatchFirNode.
+impl BatchFirNode {
+    fn run(&mut self, input: Vec<Complex<i16>>) -> Vec<Complex<i16>> {
+        let mut output = Vec::new();
+        for sample in input {
+            self.state.rotate_right(1);
+            self.state[0] = sample;
+            output.push(self.taps.iter().zip(self.state.iter())
+                .map(|(x, y)| *x * *y).sum());
+        };
+        output
+    }
+}
+
 /// Constructs a new `FirNode<T>`.
 ///
 /// Arguments:
-///     taps  - FIR filter tap Vec[T].
+///     taps  - FIR filter tap Vec[Complex<i16>].
 ///     state - Initial state for the internal filter state and memory.
 pub fn fir(taps: Vec<Complex<i16>>, state: Vec<Complex<i16>>) -> FirNode {
     FirNode::new(taps, state)
+}
+
+/// Constructs a new `BatchFirNode<T>`.
+///
+/// Arguments:
+///     taps  - FIR filter tap Vec[Complex<i16>].
+///     state - Initial state for the internal filter state and memory.
+pub fn batch_fir(taps: Vec<Complex<i16>>,
+                 state: Vec<Complex<i16>>) -> BatchFirNode {
+    BatchFirNode::new(taps, state)
 }
 
 #[cfg(test)]
@@ -103,6 +136,91 @@ mod test {
                     );
                 } else {
                     node.state.push(x);
+                }
+            }
+        );
+
+        let mut check_node = CheckNode::new(Vec::new());
+
+        connect_nodes!(source, mynode, input);
+        connect_nodes!(mynode, check_node, recv);
+        start_nodes!(source, mynode);
+        let check = thread::spawn(move || {
+            let now = Instant::now();
+            loop {
+                check_node.call();
+                if now.elapsed().subsec_millis() > 10 {
+                    break;
+                }
+            }
+        });
+        assert!(check.join().is_ok());
+    }
+
+    #[test]
+    // A test to verify the BatchFirNode correctly implements an FIR filter.
+    fn test_batch_fir_node() {
+        create_node!(
+            SomeSamples: Vec<Complex<i16>>,
+            [samples: Vec<Complex<i16>>],
+            [],
+            |node: &mut Self| {
+                let mut s1 = Complex::zero();
+                let mut s2 = Complex::zero();
+                match node.samples.pop() {
+                    Some(T) => s1 = T
+                };
+                match node.samples.pop() {
+                    Some(T) => s2 = T
+                };
+                vec![s1, s2]
+            });
+
+        let mut source = SomeSamples::new(vec![Complex::new(1, 2),
+                                               Complex::new(3, 4),
+                                               Complex::new(5, 6),
+                                               Complex::new(7, 8),
+                                               Complex::new(9, 0),
+                                               Complex::zero(),
+                                               Complex::zero(),
+                                               Complex::zero(),
+                                               Complex::zero(),
+                                               Complex::zero()]);
+
+        let mut mynode = fir_node::batch_fir(vec![Complex::new(9, 0),
+                                                  Complex::new(8, 7),
+                                                  Complex::new(6, 5),
+                                                  Complex::new(4, 3),
+                                                  Complex::new(2, 1)],
+                                             vec![Complex::zero(),
+                                                  Complex::zero(),
+                                                  Complex::zero(),
+                                                  Complex::zero(),
+                                                  Complex::zero()]);
+
+        create_node!(
+            CheckNode: (),
+            [state: Vec<Complex<i16>>],
+            [recv: Vec<Complex<i16>>],
+            |node: &mut CheckNode, x| {
+
+                if node.state.len() == 10 {
+                    assert_eq!(
+                        node.state,
+                        vec![Complex::new(9, 18),
+                             Complex::new(21, 59),
+                             Complex::new(37, 124),
+                             Complex::new(57, 205),
+                             Complex::new(81, 204),
+                             Complex::new(78, 196),
+                             Complex::new(62, 115),
+                             Complex::new(42, 50),
+                             Complex::new(18, 9),
+                             Complex::new(0, 0)
+                        ]
+                    );
+                } else {
+                    node.state.append(x);
                 }
             }
         );
