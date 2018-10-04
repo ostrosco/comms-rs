0fc6859a63bdc0ec40d8b2a1d796163f474eeade
ostrosco <ostrosco@fastmail.fm>
2018-10-03 21:49:33 -0600

    Adding more documentation for hardware traits and nodes

--- src/hardware/radio.rs
+++ src/hardware/radio.rs
@@ -1,15 +1,21 @@
 use crossbeam::{Receiver, Sender};
 use node::Node;
 
+/// A trait to capture the ability to send samples out of the hardware
+/// platform on a particular output.
 pub trait RadioTx<T> {
     fn send_samples(&mut self, samples: Vec<T>, output_idx: usize);
 }
 
+/// A trait to capture the ability to receive samples from the hardware
+/// platform on a particular input.
 pub trait RadioRx<T> {
     fn recv_samples(&mut self, num_samples: usize, input_idx: usize) -> Vec<T>;
 }
 
 create_node!(
+    #[doc = "A node that takes a generic hardware platform that supports "]
+    #[doc = "transmissing samples."]
     RadioTxNode<T, U>: (),
     [radio: T, output_idx: usize],
     [recv_samp: Vec<U>],
@@ -20,6 +26,8 @@ create_node!(
 );
 
 create_node!(
+    #[doc = "A node that takes a generic hardware platform that supports "]
+    #[doc = "receiving samples."]
     RadioRxNode<T, U>: Vec<U>,
     [radio: T, input_idx: usize, num_samples: usize],
     [],
