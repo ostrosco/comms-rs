a488e862a1390f89747c14fc4eaa212573130560
ostrosco <ostrosco@fastmail.fm>
2018-11-22 14:04:01 -0700

    Adding some comments to the FM radio example

--- examples/fm_radio.rs
+++ examples/fm_radio.rs
@@ -12,6 +12,8 @@ use num::Zero;
 use std::thread;
 
 fn main() {
+    // Get the radio frequency (assumed to be MHz as an input) and convert to
+    // Hz. If no input is specified, default to 88.7 MHz.
     let radio_mhz = std::env::args()
         .nth(1)
         .and_then(|s| str::parse::<f32>(&s).ok());
@@ -23,6 +25,7 @@ fn main() {
         }
     };
 
+    // The taps for the first low pass filter before FM demodulation.
     #[cfg_attr(rustfmt, rustfmt_skip)]
     let taps = [
 	   -5.94491898e-05, -6.29398695e-05,  1.36535572e-04,  2.23759914e-04,
@@ -42,9 +45,10 @@ fn main() {
         1.02819068e-03, -3.31926564e-05, -5.66615829e-04, -1.49209793e-04,
         2.23759914e-04,  1.36535572e-04, -6.29398695e-05, -5.94491898e-05
     ];
-    let taps_complex: Vec<Complex<f32>> =
+    let taps_samples: Vec<Complex<f32>> =
         taps.iter().map(|&x| Complex::new(x, 0.0)).collect();
 
+    // The taps for the second low pass filter after FM demodulation.
     #[cfg_attr(rustfmt, rustfmt_skip)]
     let taps_audio = [
        -7.80759301e-19, -4.70562469e-04, -7.18601045e-04, -5.64787938e-04,
@@ -89,6 +93,8 @@ fn main() {
             )).collect()
     );
 
+    // A simple node to decimate the input by dec_rate. Once decimation makes
+    // it to the standard libary, this node should go away.
     create_node!(
         DecimateNode<T>: Vec<T>,
         [dec_rate: usize],
@@ -106,6 +112,9 @@ fn main() {
         T: Copy,
     );
 
+    // Since the FIR filter currently only supports Complex samples, we need
+    // to transform our real data after demodulation into Complex samples
+    // so we can filter again.
     create_node!(
         Convert2Node: Vec<Complex<f32>>,
         [],
@@ -115,6 +124,8 @@ fn main() {
         },
     );
 
+    // After the filter, we need to convert the data bact to real samples
+    // to pass through decimation and to the audio sink.
     create_node!(
         Convert3Node: Vec<f32>,
         [],
@@ -127,7 +138,7 @@ fn main() {
     let mut sdr = radio::RadioRxNode::new(rtlsdr, 0, 262144);
     let mut convert = ConvertNode::new();
     let mut dec1: DecimateNode<Complex<f32>> = DecimateNode::new(5);
-    let mut filt1: BatchFirNode<f32> = batch_fir(taps_complex);
+    let mut filt1: BatchFirNode<f32> = batch_fir(taps_samples);
     let mut fm = fm::FMDemodNode::new(Complex::zero());
     let mut convert2 = Convert2Node::new();
     let mut filt2: BatchFirNode<f32> = batch_fir(taps_audio);
