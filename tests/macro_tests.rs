4ad43fd9e0cb6c700ed39e72c9f6847d7c7e4a01
ostrosco <ostrosco@fastmail.fm>
2019-02-10 10:37:45 -0700

    Work towards auto-generating sender and receiver structures in the macro
    - To allow for auto-generation of sender and receiver structures, we
      need to be able to crate the structure body and move from a Derive
      macro to a simple procedural macro.

--- tests/macro_tests.rs
+++ tests/macro_tests.rs
@@ -6,40 +6,37 @@ extern crate comms_rs;
 use comms_rs::prelude::*;
 use std::thread;
 
+node_derive!(
+    pub struct Node1 {
+        sender: Vec<(Sender<u32>, Option<u32>)>,
+    }
+);
+
+impl Node1 {
+    fn run(&mut self) -> u32 {
+        1
+    }
+}
+
+node_derive!(
+    pub struct Node2 {
+        recv_u32: Option<Receiver<u32>>,
+    }
+);
+
+impl Node2 {
+    fn run(&mut self, x: u32) -> () {
+        assert_eq!(x, 1);
+    }
+}
+
 #[test]
 fn test_macro() {
-    #[derive(Node)]
-    struct Node1 {
-        #[send] sender: Vec<(Sender<u32>, Option<u32>)>,
-    }
+    let mut node1 = Node1 { sender: vec![] };
 
-    impl Node1 {
-        fn run(&mut self) -> u32 {
-            1
-        }
-    }
-            
+    let mut node2 = Node2 { recv_u32: None };
 
-    let mut node1 = Node1 {
-        sender: vec![],
-    };
-
-    #[derive(Node)]
-    struct Node2 {
-        #[recv] input: Option<Receiver<u32>>,
-    }
-
-    impl Node2 {
-        fn run(&mut self, x: u32) -> () {
-            assert_eq!(x, 1);
-        }
-    }
-
-    let mut node2 = Node2 {
-        input: None,
-    };
-
-    connect_nodes!(node1, node2, input);
+    connect_nodes!(node1, node2, recv_u32);
 
     thread::spawn(move || {
         node1.call();
