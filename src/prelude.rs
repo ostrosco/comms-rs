967bda17050243df3f75512ad3e294f4559bffd9
ostrosco <ostrosco@fastmail.fm>
2019-02-11 20:26:41 -0700

    Changed macro to a proc_macro_derive
    - Since the macro is no longer generating the structure, we can now move
      to a derive macro which provides a much cleaner interface.
    - Added the #[aggregate] attribute to allow the user to specify whether
      their node should only broadcast on a Some(T) from the run() function,
      otherwise don't transmit anything.
    - Updated the FFT node to use the new implementation for testing.

--- src/prelude.rs
+++ src/prelude.rs
@@ -4,7 +4,7 @@ pub use crate::node::Node;
 pub use crate::node::NodeError;
 pub use crossbeam::{channel, Receiver, Sender};
 pub use std::thread;
-pub use node_derive::node_derive;
+pub use node_derive::Node;
 
 pub type NodeReceiver<T> = Option<Receiver<T>>;
 pub type NodeSender<T> = Vec<(Sender<T>, Option<T>)>;
