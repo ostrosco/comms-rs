bf1b9f36547f62e608c9a68f47475a5030840425
rfdoell <rfdoell@users.noreply.github.com>
2019-04-13 15:43:27 -0600

    Add additional documents to `analog_node`

--- src/modulation/analog.rs
+++ src/modulation/analog.rs
@@ -1,8 +1,10 @@
-//! This module implements frequency demodulation
+//! This module implements frequency demodulation routines for analog signals.
 use num::Complex;
 use num::Float;
 use num::Zero;
 
+/// This struct enables state persistence for frequency demodulation and instantaneous frequency
+/// estimation.
 pub struct FM<T> {
     prev: Complex<T>,
 }
@@ -11,6 +13,12 @@ impl<T> FM<T>
 where
     T: Float + Zero,
 {
+    /// This function implements batch frequency demodulation for complex samples and returns a
+    /// batch of demodulated real samples.
+    ///
+    /// # Arguments
+    ///
+    /// * `samples` - The set of complex, frequency modulated samples to demodulate
     pub fn demod(&mut self, samples: &[Complex<T>]) -> Vec<T> {
         let mut prev = self.prev;
         let mut demod_queue: Vec<T> = Vec::with_capacity(samples.len());
@@ -30,6 +38,8 @@ impl<T> Default for FM<T>
 where
     T: Float + Zero,
 {
+    /// Implements the default method for instantiating the FM node, initializing the previous
+    /// sample to zero. A user should never have to modify this value.
     fn default() -> Self {
         FM {
             prev: Complex::<T>::zero(),
