6eec2dd41c8761219e1a9d00f06f76ef259fa3aa
Kyle Harris <kaharrs2@gmail.com>
2019-10-23 20:34:30 -0600

    Initial implementation of QPSK symbol mapping, also added tests

--- src/modulation/digital.rs
+++ src/modulation/digital.rs
@@ -1,31 +1,158 @@
 //! Provide tools to do digital modulation
 
-extern crate num_complex;
+use num::complex::Complex;
 
 /// Modulates a bit to a complex int16 impulse via BPSK
-pub fn bpsk_bit_mod(bit: u8) -> Option<num_complex::Complex<i16>> {
-    if bit == 1 {
-        Some(num_complex::Complex::new(1, 0))
-    } else if bit == 0 {
-        Some(num_complex::Complex::new(-1, 0))
+pub fn bpsk_bit_mod(bit: u8) -> Option<Complex<i16>> {
+    if bit == 0 {
+        Some(Complex::new(1, 0))
+    } else if bit == 1 {
+        Some(Complex::new(-1, 0))
     } else {
         None
     }
 }
 
-/// Modulates a byte via BPSK into int16 samples
-pub fn bpsk_byte_mod(byte: u8) -> Vec<num_complex::Complex<i16>> {
-    let mut mod_data = Vec::with_capacity(8);
-    let first_bit = 0b0001 & byte;
-    let second_bit = (0b0010 & byte).rotate_right(1);
-    let third_bit = (0b0100 & byte).rotate_right(2);
-    let fourth_bit = (0b1000 & byte).rotate_right(3);
+/// Modulates a byte via BPSK into 8 int16 samples
+pub fn bpsk_byte_mod(byte: u8) -> Vec<Complex<i16>> {
+    (0..8)
+        .map(|i| bpsk_bit_mod(((1_u8 << i) & byte).rotate_right(i)).unwrap())
+        .collect()
+}
 
-    // We know that this can't be greater than 1, because of explicit bit twiddling
-    mod_data[0] = bpsk_bit_mod(first_bit).unwrap();
-    mod_data[1] = bpsk_bit_mod(second_bit).unwrap();
-    mod_data[2] = bpsk_bit_mod(third_bit).unwrap();
-    mod_data[3] = bpsk_bit_mod(fourth_bit).unwrap();
+/// Modulates a pair of bits to a complex int16 impulse via QPSK
+pub fn qpsk_bit_mod(bits: u8) -> Option<Complex<i16>> {
+    if bits == 0 {
+        Some(Complex::new(1, 1))
+    } else if bits == 1 {
+        Some(Complex::new(-1, 1))
+    } else if bits == 2 {
+        Some(Complex::new(1, -1))
+    } else if bits == 3 {
+        Some(Complex::new(-1, -1))
+    } else {
+        None
+    }
+}
 
-    mod_data
+/// Modulates a byte via QPSK into 4 int16 samples
+pub fn qpsk_byte_mod(byte: u8) -> Vec<Complex<i16>> {
+    (0..8)
+        .step_by(2)
+        .map(|i| qpsk_bit_mod(((3_u8 << i) & byte).rotate_right(i)).unwrap())
+        .collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use num::Complex;
+
+    #[test]
+    fn test_bpsk_bit() {
+        assert_eq!(bpsk_bit_mod(0_u8).unwrap(), Complex::new(1, 0));
+        assert_eq!(bpsk_bit_mod(1_u8).unwrap(), Complex::new(-1, 0));
+    }
+
+    #[test]
+    fn test_qpsk_bit() {
+        assert_eq!(qpsk_bit_mod(0_u8).unwrap(), Complex::new(1, 1));
+        assert_eq!(qpsk_bit_mod(1_u8).unwrap(), Complex::new(-1, 1));
+        assert_eq!(qpsk_bit_mod(2_u8).unwrap(), Complex::new(1, -1));
+        assert_eq!(qpsk_bit_mod(3_u8).unwrap(), Complex::new(-1, -1));
+    }
+
+    #[test]
+    fn test_bpsk_byte() {
+        assert_eq!(bpsk_byte_mod(0_u8), vec![Complex::new(1, 0); 8]);
+        assert_eq!(
+            bpsk_byte_mod(31_u8),
+            vec![
+                Complex::new(-1, 0),
+                Complex::new(-1, 0),
+                Complex::new(-1, 0),
+                Complex::new(-1, 0),
+                Complex::new(-1, 0),
+                Complex::new(1, 0),
+                Complex::new(1, 0),
+                Complex::new(1, 0)
+            ]
+        );
+        assert_eq!(
+            bpsk_byte_mod(63_u8),
+            vec![
+                Complex::new(-1, 0),
+                Complex::new(-1, 0),
+                Complex::new(-1, 0),
+                Complex::new(-1, 0),
+                Complex::new(-1, 0),
+                Complex::new(-1, 0),
+                Complex::new(1, 0),
+                Complex::new(1, 0)
+            ]
+        );
+        assert_eq!(
+            bpsk_byte_mod(127_u8),
+            vec![
+                Complex::new(-1, 0),
+                Complex::new(-1, 0),
+                Complex::new(-1, 0),
+                Complex::new(-1, 0),
+                Complex::new(-1, 0),
+                Complex::new(-1, 0),
+                Complex::new(-1, 0),
+                Complex::new(1, 0)
+            ]
+        );
+        assert_eq!(bpsk_byte_mod(255_u8), vec![Complex::new(-1, 0); 8]);
+    }
+
+    #[test]
+    fn test_qpsk_byte() {
+        assert_eq!(
+            qpsk_byte_mod(0_u8),
+            vec![
+                Complex::new(1, 1),
+                Complex::new(1, 1),
+                Complex::new(1, 1),
+                Complex::new(1, 1)
+            ]
+        );
+        assert_eq!(
+            qpsk_byte_mod(2_u8),
+            vec![
+                Complex::new(1, -1),
+                Complex::new(1, 1),
+                Complex::new(1, 1),
+                Complex::new(1, 1)
+            ]
+        );
+        assert_eq!(
+            qpsk_byte_mod(4_u8),
+            vec![
+                Complex::new(1, 1),
+                Complex::new(-1, 1),
+                Complex::new(1, 1),
+                Complex::new(1, 1)
+            ]
+        );
+        assert_eq!(
+            qpsk_byte_mod(15_u8),
+            vec![
+                Complex::new(-1, -1),
+                Complex::new(-1, -1),
+                Complex::new(1, 1),
+                Complex::new(1, 1)
+            ]
+        );
+        assert_eq!(
+            qpsk_byte_mod(254_u8),
+            vec![
+                Complex::new(1, -1),
+                Complex::new(-1, -1),
+                Complex::new(-1, -1),
+                Complex::new(-1, -1)
+            ]
+        );
+    }
 }
