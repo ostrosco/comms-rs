d1b36b904c49f766e872d2893e5efe5d2cff51cf
Kyle Harris <kaharrs2@gmail.com>
2021-05-02 12:21:40 -0600

    Need to keep the ActiveClient from dropping I think

--- examples/jack_sine.rs
+++ examples/jack_sine.rs
@@ -3,8 +3,68 @@ extern crate comms_rs;
 extern crate rodio;
 use comms_rs::io::jack_node::JackOutputNode;
 use comms_rs::prelude::*;
-use rodio::source::{self, Source};
+use rodio::source::Source;
 use std::sync::Arc;
+use std::f32::consts::PI;
+use std::time::Duration;
+
+#[derive(Clone, Debug)]
+pub struct SineWaveArb {
+    freq: f32,
+    sample_rate: u32,
+    num_sample: usize,
+    cntr: u32,
+}
+
+impl SineWaveArb {
+    #[inline]
+    pub fn new(freq: u32, sample_rate: u32) -> SineWaveArb {
+        SineWaveArb {
+            freq: freq as f32,
+            sample_rate: sample_rate,
+            num_sample: 0,
+            cntr: 0,
+        }
+    }
+}
+
+impl Iterator for SineWaveArb {
+    type Item = f32;
+
+    #[inline]
+    fn next(&mut self) -> Option<f32> {
+        self.num_sample = self.num_sample.wrapping_add(1);
+        let value = 2.0 * PI * self.freq * self.num_sample as f32 / (self.sample_rate as f32);
+        self.cntr += 1;
+        if self.cntr > 1000 {
+            println!("value.sin(): {:?}", value.sin());
+            self.cntr = 0;
+        }
+        Some(value.sin())
+    }
+}
+
+impl Source for SineWaveArb {
+    #[inline]
+    fn current_frame_len(&self) -> Option<usize> {
+        None
+    }
+
+    #[inline]
+    fn channels(&self) -> u16 {
+        1
+    }
+
+    #[inline]
+    fn sample_rate(&self) -> u32 {
+        self.sample_rate
+    }
+
+    #[inline]
+    fn total_duration(&self) -> Option<Duration> {
+        None
+    }
+}
 
 fn main() {
     let mut jack_output: JackOutputNode = JackOutputNode::new();
@@ -13,6 +73,7 @@ fn main() {
     struct SineNode {
         source: Box<dyn Source<Item = f32> + Send>,
         pub output: NodeSender<f32>,
+        pub cntr: u32,
     }
 
     impl SineNode {
@@ -20,12 +81,18 @@ fn main() {
             SineNode {
                 source,
                 output: Default::default(),
+                cntr: 0,
             }
         }
 
         pub fn run(&mut self) -> Result<f32, NodeError> {
             let source = &mut self.source;
             if let Some(samp) = source.next() {
+                self.cntr += 1;
+                if self.cntr >= 1000 {
+                    println!("samp: {:?}", samp);
+                    self.cntr = 0;
+                }
                 Ok(samp)
             } else {
                 Ok(0.0)
@@ -33,10 +100,9 @@ fn main() {
         }
     }
 
-    let mut sine = SineNode::new(Box::new(source::SineWave::new(440)));
+    let mut sine = SineNode::new(Box::new(SineWaveArb::new(440, 44100)));
 
-    //connect_nodes!(sine, output, jack_output, input);
-    let (send, recv) = channel::unbounded();
+    let (send, recv) = channel::bounded(4410);
     sine.output.push((send, None));
     jack_output.input = Arc::new(Some(recv));
     start_nodes!(sine, jack_output);
--- src/io/jack_node.rs
+++ src/io/jack_node.rs
@@ -1,23 +1,30 @@
 use crate::prelude::*;
 use jack::{Client, ClosureProcessHandler, Control, ProcessScope};
 use std::sync::Arc;
+use crossbeam::crossbeam_channel::{unbounded};
+use std::fs::File;
 
 #[derive(Default)]
-pub struct JackOutputNode {
+pub struct JackOutputNode<P> {
     pub input: Arc<NodeReceiver<f32>>,
     pub sample_rate: usize,
+    pub cb_rx: Option<Receiver<String>>,
+    pub active_client: Option<jack::AsyncClient<(), P>>,
 }
 
-impl JackOutputNode {
+impl<P> JackOutputNode<P> {
     pub fn new() -> Self {
         JackOutputNode {
             input: Default::default(),
             sample_rate: 0,
+            cb_rx: None,
+            active_client: None,
         }
     }
 }
 
-impl Node for JackOutputNode {
+impl<P: Send> Node for JackOutputNode<P>
+{
     fn start(&mut self) {
         // 1. Open client
         let (client, _status) =
@@ -31,50 +38,59 @@ impl Node for JackOutputNode {
 
         // 3. Callback definition
         self.sample_rate = client.sample_rate();
-        println!("Sample Rate: {:?} Hz", self.sample_rate);
         let xbeam_channel: Arc<_> = self.input.clone();
+        let (cb_send, cb_rx) = unbounded();
+        self.cb_rx = Some(cb_rx);
 
-        let process = ClosureProcessHandler::new(
-            // Callback function signature is basically non-negotiable I think...
-            move |_cl: &Client, ps: &ProcessScope| -> Control {
-                // Get the output buffer
-                let out = out_port.as_mut_slice(ps);
-                let mut out_iter = out.iter_mut();
+        // Callback function signature is basically non-negotiable I think...
+        let process_cb = move |_cl: &Client, ps: &ProcessScope| -> Control {
+            // Get the output buffer
+            let out = out_port.as_mut_slice(ps);
+            let mut out_iter = out.iter_mut();
 
-                // TODO: Do the samples at the input get consumed properly as
-                // we iterate over them here?
-                // Get the crossbeam channel
-                for sample in (*xbeam_channel).as_ref().unwrap() {
-                    // Write output
-                    if let Some(o) = out_iter.next() {
-                        *o = sample;
-                    } else {
-                        // Get here if # of samples ready at input > out_len...
-                        // Continue as normal
-                        return Control::Continue;
-                    }
+            // TODO: Do the samples at the input get consumed properly as
+            // we iterate over them here?
+            // Get the crossbeam channel
+            let mut file = File::create("/home/styty/git/comms-rs/i_am_here").unwrap();
+            let msg = format!("xbeam_channel len: {:?}", (*xbeam_channel).as_ref().unwrap().len());
+            cb_send.send(String::from("herp")).unwrap();
+            for sample in (*xbeam_channel).as_ref().unwrap() {
+                // Write output
+                if let Some(o) = out_iter.next() {
+                    *o = sample;
+                } else {
+                    // Get here if # of samples ready at input > out_len...
+                    // Continue as normal
+                    return Control::Continue;
                 }
+            }
 
-                // Get here if out_len > # of samples ready at input...
-                // Continue as normal
-                Control::Continue
-            },
-        );
+            // Get here if out_len > # of samples ready at input...
+            // Continue as normal
+            Control::Continue
+        };
 
         // 4. Activate client
-        let _active_client = client.activate_async((), process).unwrap();
+        let process = jack::ClosureProcessHandler::new(process_cb);
+        self.active_client = Some(client.activate_async((), process).unwrap());
 
         // 5. Processing...
 
         // 6. Optional client deactivate
         //active_client.deactivate().uwrap();
-        //
+
+        println!("Sample Rate: {:?} Hz", self.sample_rate);
     }
 
     fn call(&mut self) -> Result<(), NodeError> {
         // JACK drives the sample copying with the callback we register in the
         // JACK server, meaning comms-rs really doesn't do anything in it's run
         // handling
+        if let Some(cb_rx) = &self.cb_rx {
+            if let Ok(msg) = cb_rx.try_recv() {
+                println!("msg from cb: {:?}", msg);
+            }
+        }
         Ok(())
     }
 
